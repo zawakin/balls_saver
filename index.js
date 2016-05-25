49b1e1516dc39d4a2eeefbd03f39eb2a960353c4
Yuki Miyake <zawawabis@gmail.com>
2016-05-26 07:58:29 +0900

    BallsIOクラス追加。録画中にcounterが止まった場合の処理が未実装。

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var fs = require("fs");
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+
 app.set("port", 8000);
 app.use(express.static("balls"));
 app.get("/", function (req, res) {
     res.sendFile("index.html");
+});
+
+app.post("/", function(req,res){
+    res.send(load_data_array(res));
 });
 
 function save_data_array(ws,sdatas){
@@ -17,30 +28,19 @@ function save_data_array(ws,sdatas){
 
 function load_data_array(){
   var res = fs.readFileSync("hoge.txt","utf-8")
-
-  // (err, text){
-  //   res = text;
-  //   console.log("1" + err);
-  //   console.log("2" + text);
-  //   console.log("3" + res);
-  // });
-  // console.log("4" + res);
-  // console.log(res);
   return res;
-
 }
 
 var sdatas = [];
   io.on("connection", function(socket){
-    socket.emit("greeting","hello");
     console.log("new socket connected.");
 
+    socket.on("send_start", function(){
+        sdatas = [];
+    });
     socket.on("send_data", function(sdata){
-      // console.log(data.mouse + " " + data.keyCode1.);
       data = JSON.parse(sdata);
       sdatas.push(sdata);
-      var keyCode1 = data.k1;
-      // console.log(keyCode1.length + " " + data.mouse.x + " " + data.mouse.y);
     });
 
     save_ended = false;
@@ -50,22 +50,15 @@ var sdatas = [];
         save_data_array(ws,sdatas);
         save_ended = true;
         console.log("saving successfully.");
-        stext = load_data_array();
-        console.log("load_data sent.")
-        socket.emit("load_data", stext);
       }
     });
 
     socket.on("load_request",function(some_obj){
-      console.log("got load_request.");
+      console.log("load request received.");
       socket.emit("load_data", load_data_array());
-      console.log("send data.");
+      console.log("alldata sent.");
     });
-
-
   });
-
-
 
 http.listen(app.get("port"), function () {
     console.log("port = %s", app.get("port"));
